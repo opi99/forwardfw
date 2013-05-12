d0f4dabf5dbbadf918ff052672b83f6265f6ef43
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2013-05-12 10:35:50 +0000

    PSR2 compatibility

--- trunk/forwardfw/src/ForwardFW/Controller/Application.php
+++ trunk/forwardfw/src/ForwardFW/Controller/Application.php
@@ -1,5 +1,4 @@
 <?php
-declare(encoding = "utf-8");
 /**
  * This file is part of ForwardFW a web application framework.
  *
@@ -23,9 +22,8 @@ declare(encoding = "utf-8");
  * @package    ForwardFW
  * @subpackage Controller
  * @author     Alexander Opitz <opitz.alexander@primacom.net>
- * @copyright  2009-2010 The Authors
+ * @copyright  2009-2013 The Authors
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
- * @version    SVN: $Id: $
  * @link       http://forwardfw.sourceforge.net
  * @since      File available since Release 0.0.1
  */
@@ -231,4 +229,3 @@ class ForwardFW_Controller_Application extends ForwardFW_Controller_View
         return $this->arScreens;
     }
 }
-?>
