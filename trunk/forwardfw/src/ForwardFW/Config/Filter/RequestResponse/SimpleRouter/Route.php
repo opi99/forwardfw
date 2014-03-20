bd724205ccc8110eae7fdfda792891b1fc798c52
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2014-03-20 08:11:08 +0000

    Config for simple routing

---
+++ trunk/forwardfw/src/ForwardFW/Config/Filter/RequestResponse/SimpleRouter/Route.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * This file is part of ForwardFW a web application framework.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * PHP version 5
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @copyright  2009-2014 The Authors
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ * @since      File available since Release 0.0.11
+ */
+
+namespace ForwardFW\Config\Filter\RequestResponse\SimpleRouter;
+
+/**
+ * Config for a SimpleRouter Filter.
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ */
+class Route extends \ForwardFW\Config
+{
+    /**
+     * @var string Startpoint of the route
+     */
+    private $strStart = '';
+
+    /**
+     * @var string FilterClass to call
+     */
+    private $strFilterClass = '';
+
+    /**
+     * @var ForwardFW\Config\Filter\RequestResponse Config of the filter
+     */
+    private $filterConfig = '';
+
+    /**
+     * Sets Startpoint of the route
+     *
+     * @param string $strStart Startpoint of the route
+     *
+     * @return ForwardFW\Config\Filter\RequestResponse\SimpleRouter
+     */
+    public function setStart($strStart)
+    {
+        $this->strStart = $strStart;
+        return $this;
+    }
+
+    /**
+     * Sets Classame of the RequestResponse filter to add in FilterChain
+     *
+     * @param string $strFilterClass Classname of the RequestResponse filter to add in FilterChain
+     *
+     * @return ForwardFW\Config\Filter\RequestResponse\SimpleRouter
+     */
+    public function setFilterClass($strFilterClass)
+    {
+        $this->strFilterClass = $strFilterClass;
+        return $this;
+    }
+
+    /**
+     * Config of the RequestResponse filter
+     *
+     * @param ForwardFW\Config\Filter\RequestResponse $filterConfig Config of the RequestResponse filter.
+     *
+     * @return ForwardFW\Config\Filter\RequestResponse\SimpleRouter
+     */
+    public function setFilterConfig(\ForwardFW\Config\Filter\RequestResponse $filterConfig)
+    {
+        $this->filterConfig = $filterConfig;
+        return $this;
+    }
+
+    /**
+     * Get Startpoint of the route.
+     *
+     * @return string
+     */
+    public function getStart()
+    {
+        return $this->strStart;
+    }
+
+    /**
+     * Get Classame of the RequestResponse filter.
+     *
+     * @return string
+     */
+    public function getFilterClass()
+    {
+        return $this->strFilterClass;
+    }
+
+
+    /**
+     * Get config of the RequestResponse filter.
+     *
+     * @return ForwardFW\Config\Filter\RequestResponse
+     */
+    public function getFilterConfig()
+    {
+        return $this->filterConfig;
+    }
+}
