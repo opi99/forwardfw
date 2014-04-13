7f8d8c58f447af26053fbf1a3e7786053edea993
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2014-04-13 10:07:12 +0000

    Update copyright year.

--- trunk/forwardfw/src/ForwardFW/Cache/Exception/IsGenerating.php
+++ trunk/forwardfw/src/ForwardFW/Cache/Exception/IsGenerating.php
@@ -22,7 +22,7 @@
  * @package    ForwardFW
  * @subpackage Cache
  * @author     Alexander Opitz <opitz.alexander@primacom.net>
- * @copyright  2009-2013 The Authors
+ * @copyright  2009-2014 The Authors
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @link       http://forwardfw.sourceforge.net
  * @since      File available since Release 0.0.9
