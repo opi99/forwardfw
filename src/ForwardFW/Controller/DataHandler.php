5151c63e849b14315dcc94fa8fac96277040230c
Alexander Opitz <opitz.alexander@googlemail.com>
2015-06-02 16:50:38 +0000

    Cleanup DataHandler and add callback for id of newly created elements.

--- src/ForwardFW/Controller/DataHandler.php
+++ src/ForwardFW/Controller/DataHandler.php
@@ -22,7 +22,7 @@
  * @package    ForwardFW
  * @subpackage Controller
  * @author     Alexander Opitz <opitz.alexander@primacom.net>
- * @copyright  2009-2014 The Authors
+ * @copyright  2009-2015 The Authors
  * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
  * @link       http://forwardfw.sourceforge.net
  * @since      File available since Release 0.0.7
@@ -31,7 +31,7 @@
 namespace ForwardFW\Controller;
 
 /**
- * Managing DataLoading via PEAR::MDB
+ * Factory(?) Holder of DataHandler handlers.
  *
  * @category   Application
  * @package    ForwardFW
@@ -45,7 +45,7 @@ class DataHandler extends \ForwardFW\Controller implements DataHandlerInterface
     /**
      * @var array Cache of connections
      */
-    protected $arConnectionCache = array();
+    protected $connectionCache = array();
 
     /**
      * Constructor
@@ -80,20 +80,20 @@ class DataHandler extends \ForwardFW\Controller implements DataHandlerInterface
     /**
      * Loads Data from cache or from a connection (DB, SOAP, File) if cache failed.
      *
-     * @param string $strConnection Name of connection
-     * @param array  $arOptions     Options to load the data
+     * @param string $connectionName Name of connection
+     * @param array $options Options to load the data
      *
      * @return mixed Data from the connection.
      */
-    public function loadFromCached($strConnection, array $arOptions, $nCacheTimeout = -1)
+    public function loadFromCached($connectionName, array $options, $nCacheTimeout = -1)
     {
-        $handler = $this->getConnection($strConnection);
+        $handler = $this->getConnection($connectionName);
 
-        $cache = $this->getCacheSystem($strConnection);
+        $cache = $this->getCacheSystem($connectionName);
 
         $cacheCallback = new \ForwardFW\Callback(
             array($handler, 'loadFrom'),
-            array($strConnection, $arOptions)
+            array($connectionName, $options)
         );
 
         $configCacheData = new \ForwardFW\Config\Cache\Data\Caller();
@@ -109,11 +109,11 @@ class DataHandler extends \ForwardFW\Controller implements DataHandlerInterface
      * Initializes and returns the caching system depending on connection
      * configuration.
      *
-     * @param string $strConnection Name of connection
+     * @param string $connectionName Name of connection
      *
      * @return ForwardFW_Cache_Frontend The Cache Frontend.
      */
-    protected function getCacheSystem($strConnection)
+    protected function getCacheSystem($connectionName)
     {
         $backendConfig = new \ForwardFW\Config\Cache\Backend\File();
         $backendConfig->strPath = getcwd() . '/cache/';
@@ -135,97 +135,94 @@ class DataHandler extends \ForwardFW\Controller implements DataHandlerInterface
     /**
      * Loads Data from a connection (DB, SOAP, File)
      *
-     * @param string $strConnection Name of connection
-     * @param array  $arOptions     Options to load the data
+     * @param string $connectionName Name of connection
+     * @param array $options Options to load the data
      *
      * @return mixed Data from the connection.
      */
-    public function loadFrom($strConnection, array $arOptions)
+    public function loadFrom($connectionName, array $options)
     {
-        $handler = $this->getConnection($strConnection);
-        return $handler->loadFrom($strConnection, $arOptions);
+        $handler = $this->getConnection($connectionName);
+        return $handler->loadFrom($connectionName, $options);
     }
 
     /**
      * Saves Data to a connection (DB, SOAP, File)
      *
-     * @param string $strConnection Name of connection
-     * @param array  $arOptions     Options to load the data
+     * @param string $connectionName Name of connection
+     * @param array $options Options to load the data
+     * @param ForwardFW\Callback $idCallback Callback to give id of object creation
      *
      * @return mixed Data from the connection.
      */
-    public function create($strConnection, array $arOptions)
+    public function create($connectionName, array $options, \ForwardFW\Callback $idCallback = null)
     {
-        $handler = $this->getConnection($strConnection);
-        return $handler->create($strConnection, $arOptions);
+        $handler = $this->getConnection($connectionName);
+        return $handler->create($connectionName, $options, $idCallback);
     }
 
     /**
      * Truncates Data to a connection (DB, SOAP, File)
      *
-     * @param string $strConnection Name of connection
-     * @param array  $arOptions     Options to load the data
+     * @param string $connectionName Name of connection
+     * @param array $options Options to load the data
      *
      * @return mixed Data from the connection.
      */
-    public function truncate($strConnection, array $arOptions)
+    public function truncate($connectionName, array $options)
     {
-        $handler = $this->getConnection($strConnection);
-        return $handler->truncate($strConnection, $arOptions);
+        $handler = $this->getConnection($connectionName);
+        return $handler->truncate($connectionName, $options);
     }
 
     /**
      * Saves Data to a connection (DB, SOAP, File)
      *
-     * @param string $strConnection Name of connection
-     * @param array  $arOptions     Options to load the data
+     * @param string $connectionName Name of connection
+     * @param array $options Options to load the data
      *
      * @return mixed Data from the connection.
      */
-    public function saveTo($strConnection, array $arOptions)
+    public function saveTo($connectionName, array $options)
     {
-        $handler = $this->getConnection($strConnection);
-        return $handler->saveTo($strConnection, $arOptions);
+        $handler = $this->getConnection($connectionName);
+        return $handler->saveTo($connectionName, $options);
     }
 
     /**
      * Gets the connection handler.
      *
-     * @param string $strConnection Name of connection
+     * @param string $connectionName Name of connection
      *
      * @return mixed ConnectionHandler
      */
-    protected function getConnection($strConnection)
+    protected function getConnection($connectionName)
     {
-        if (!isset($this->arConnectionCache[$strConnection])) {
-            $this->initConnection($strConnection);
+        if (!isset($this->connectionCache[$connectionName])) {
+            $this->initConnection($connectionName);
         }
         // Return existing connection
-        return $this->arConnectionCache[$strConnection];
+        return $this->connectionCache[$connectionName];
     }
 
     /**
      * Loads and initialize the connection handler.
      *
-     * @param string $strConnection Name of connection
+     * @param string $connectionName Name of connection
      *
      * @return void
      */
-    public function initConnection($strConnection)
+    public function initConnection($connectionName)
     {
-        $arConfig = $this->getConfigParameter($strConnection);
-        $strHandlerClass = $arConfig['handler'];
+        $config = $this->getConfigParameter($connectionName);
+        $handlerClassName = $config['handler'];
 
-        if (isset($arConfig['prefix'])) {
-            $this->arTablePrefix[$strConnection] = $arConfig['prefix'];
-        }
-
-        if (class_exists($strHandlerClass)) {
-            $handler = new $strHandlerClass($this->application);
+        if (class_exists($handlerClassName)) {
+            $handler = new $handlerClassName($this->application);
         } else {
-            throw new \ForwardFW\Exception\DataHandler('DataHandlerClass "' . $strHandlerClass . '" not includeable.');
+            throw new \ForwardFW\Exception\DataHandler('DataHandlerClass "' . $handlerClassName . '" not includeable.');
         }
 
-        $this->arConnectionCache[$strConnection] = $handler;
+        $this->connectionCache[$connectionName] = $handler;
     }
 }
