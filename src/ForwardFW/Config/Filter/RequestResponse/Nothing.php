8e4f748de45ad22959f1b4611db44a5c7acc7db5
Alexander Opitz <opitz.alexander@googlemail.com>
2015-06-18 15:04:40 +0000

    Implement Nothing as application

---
+++ src/ForwardFW/Config/Filter/RequestResponse/Nothing.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of ForwardFW a web application framework.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * PHP version 5
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @copyright  2015 The Authors
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ * @since      File available since Release 0.1.0
+ */
+
+namespace ForwardFW\Config\Filter\RequestResponse;
+
+/**
+ * Config for Nothing.
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ */
+class Nothing extends \ForwardFW\Config\Filter\RequestResponse
+{
+    /**
+     * @var string Class of application to call
+     */
+    protected $strExecutionClass = 'ForwardFW\\Filter\\RequestResponse\\Nothing';
+}
