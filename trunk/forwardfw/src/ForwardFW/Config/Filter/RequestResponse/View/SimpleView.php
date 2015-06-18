27a1e860f750d94cd914c421233494c5e7075d41
opi <opi@329c4359-9da0-4f32-8655-818518c4f8f5>
2015-06-18 15:53:14 +0000

    Add SimpleFileViewer

---
+++ trunk/forwardfw/src/ForwardFW/Config/Filter/RequestResponse/View/SimpleView.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * This file is part of ForwardFW a web application framework.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * PHP version 5
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @copyright  2015 The Authors
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ * @since      File available since Release 0.1.0
+ */
+
+namespace ForwardFW\Config\Filter\RequestResponse\View;
+
+/**
+ * Config for a Application Filter.
+ *
+ * @category   Filter
+ * @package    ForwardFW
+ * @subpackage Config
+ * @author     Alexander Opitz <opitz.alexander@primacom.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       http://forwardfw.sourceforge.net
+ */
+class SimpleView extends \ForwardFW\Config\Filter\RequestResponse
+{
+    /**
+     * @var string Class of application to call
+     */
+    protected $strExecutionClass = 'ForwardFW\\Filter\\RequestResponse\\View\\SimpleView';
+
+    /**
+     * @var string Name of file to output
+     */
+    protected $fileName = '';
+
+    /**
+     * Sets name of file to output.
+     *
+     * @param string $fileName Name of file
+     * @return ForwardFW\Config\Filter\RequestResponse\View\SimpleView
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+        return $this;
+    }
+
+    /**
+     * Get name of file to output.
+     *
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+}
