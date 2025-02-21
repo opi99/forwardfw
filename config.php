61a69cbaaf2386659a798d67586e47ebdbd7852f
Alexander Opitz <opitz.alexander@googlemail.com>
2025-02-21 11:13:11 +0100

    Add ClockworkLogger (exclude ChromeLogger)

--- config.php
+++ config.php
@@ -9,11 +9,18 @@ return (new \ForwardFW\Config\Runner\HttpMiddlewareRunner())
                 ->setDsn('mysqli://john:doe@localhost/forwardfw')
                 ->setTablePrefix('')
         )*/
+        // ->addService(
+        //     (new \ForwardFW\Config\Service\Logger\ChromeLogger())
+        // )
         ->addService(
-            (new \ForwardFW\Config\Service\Logger\ChromeLogger())
+            (new \ForwardFW\Config\Service\Logger\ClockworkLogger())
+                ->enable()
         )
+        // ->addMiddleware(
+        //     new \ForwardFW\Config\Middleware\Logger\ChromeLogger()
+        // )
         ->addMiddleware(
-            new \ForwardFW\Config\Middleware\Logger\ChromeLogger()
+            (new \ForwardFW\Config\Middleware\Logger\ClockworkLogger())
         )
         ->addMiddleware(
             new \ForwardFW\Config\Middleware\SimpleRouter()
